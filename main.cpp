271035e4f9b07cd50b61939cb88f644302bb7e26
yangyaoming158 <yangyaoming23@gmail.com>
2026-01-05 10:03:04 +0800

    create database

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
-
+#include "src/data/dbmanager.h" // 1. 引入头文件
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // 2. 尝试初始化数据库 (这行代码会让数据库文件生成)
+    DBManager::getInstance().initDB();
     MainWindow w;
     w.show();
     return a.exec();
